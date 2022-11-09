34d08b4266e888966f0cd45f4075ff1777e14eda
Ankit Kumar Mishra <73931667+ankitmishra2232@users.noreply.github.com>
2022-11-09 22:17:38 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"vishalsiram50@gmail.com\"\n\
+email = \"ankitmishra.in.com@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
